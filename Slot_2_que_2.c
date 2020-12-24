b6c12476e4c180d9151b00d0811b068bbff2cc75
Raviraj Dulange <redhood.dumb@gmail.com>
2020-12-24 20:15:33 +0530

    Unproctored Slot 2 Question 2

---
+++ Slot_2_que_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, e, sum = 0;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    if (a == b && b == c && c == d && d == e)
+    {
+        sum = 2000 + a * 5;
+        printf("%d", sum);
+    }
+    else if (a == b && b == c || b == c && c == d)
+    {
+        sum = 1000 + a + b + c + d + e;
+        printf("%d", sum);
+    }
+    else if (a != b && b != c && c != d && d != e)
+    {
+        sum = 500 + a + b + c + d + e;
+        printf("%d", sum);
+    }
+    else
+    {
+        sum = a + b + c + d + e;
+        printf("%d", sum);
+    }
+    return 0;
+}
