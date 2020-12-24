9f001bae1af52ca336493dbd9f86607e61f4d8d5
Raviraj Dulange <redhood.dumb@gmail.com>
2020-12-24 21:14:05 +0530

    Unproctored Slot 2 Question 2

--- Slot_2_que_2.c
+++ Slot_2_que_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main()
 {
+    //Purely implementation.
     int a, b, c, d, e, sum = 0;
     scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
     if (a == b && b == c && c == d && d == e)
@@ -23,6 +24,6 @@ int main()
         sum = a + b + c + d + e;
         printf("%d", sum);
     }
-    
+
     return 0;
 }
