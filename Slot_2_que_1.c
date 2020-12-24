99c15373e8479769615668a15a3ab1b8d4e02cdc
Raviraj Dulange <redhood.dumb@gmail.com>
2020-12-24 20:12:25 +0530

    Unproctored Slot 2 Question 2

---
+++ Slot_2_que_1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char tolower1(char a) {
+	if(a >= 'A' && a<='Z')
+      a = a + 32;
+  return a;
+}
+
+int main()
+{
+   int i, j, n, m, count = 0;
+   char name[200], surname[100];
+   scanf("%s", name);
+   for(i = 0; name[i] != ':'; i++)
+   	count++;
+   for(i = 0; name[i] != '\0'; i++)
+   	name[i] = tolower1(name[i]);
+   name[0] = name[0] - 32;
+   name[count] = ':';
+  count++;
+   name[count] = name[count] - 32;
+   printf("%s\n", name);
+   return 0;
+}
